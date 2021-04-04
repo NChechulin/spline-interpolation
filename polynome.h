49cfc0712780deea8586c67375ff2bbdb9fc2363
Nikolay Chechulin <nchechulin@aol.com>
2021-04-04 18:01:36 +0300

    add Polynome class with a constructor and GetDerivative method

---
+++ polynome.h
@@ -0,0 +1,17 @@
+#ifndef POLYNOME_H
+#define POLYNOME_H
+
+
+class Polynome
+{
+public:
+    // coefficients of the polynome in the form ax^3 + bx^2 + cx + d
+    double a, b, c, d;
+    // boundaries
+    double from, to;
+
+    Polynome(double a, double b, double c, double d);
+    Polynome GetDerivative() const;
+};
+
+#endif // POLYNOME_H
